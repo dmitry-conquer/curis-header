4924f791ec2c7e07736949db0b3e0088906630db
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-11-25 10:31:06 +0200

    Implement dynamic header background change on scroll and update header styles for improved UX

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,26 @@
 import "../styles/main.scss";
 
 document.addEventListener("DOMContentLoaded", (): void => {
-  /**
-   * Code here
-   */
+  const header = document.getElementById("header-solid");
+  let ticking = false;
+
+  const updateHeader = (): void => {
+    if (window.scrollY > 100) {
+      header?.classList.add("header-fill-scrolled");
+    } else {
+      header?.classList.remove("header-fill-scrolled");
+    }
+    ticking = false;
+  };
+
+  window.addEventListener(
+    "scroll",
+    () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateHeader);
+        ticking = true;
+      }
+    },
+    { passive: true }
+  );
 });
